ff60d7a4684ac30509acb5f2314814c93aafa3b6
Severina <sevrinaCh@gmail.com>
2023-12-08 19:26:19 -0400

    Practica-1Aux

---
+++ src/Practica1/PracticaComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Pregunta1 } from "./components/Pregunta1";
+import { Pregunta2 } from "./components/Pregunta2";
+import { Student } from "./components/Pregunta3";
+import { Pregunta4 } from "./components/Pregunta4";
+import { person } from "./components/Pregunta5";
+import { Pregunta6 } from "./components/Pregunta6";
+import { Status } from "./components/Pregunta7";
+import { index } from "./components/index";
+
+export const PracticaComponent = () => {
+  return (
+    <div>
+      <center><h1 >Practica # 1 ayudantia</h1></center>
+      <Pregunta1/>
+      <hr/>
+      <Pregunta2/>
+      <hr />
+      <Student/>
+      <hr/>
+      <Pregunta4/>
+      <person/>
+      <Pregunta6/>
+      <Status/>
+      <index/>
+    </div>
+  );
+}
